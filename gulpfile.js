69627be6a8dcc8143df0bd038ea4f7c73d7f65f9
Guilherme Niero <gniero@gmail.com>
2017-03-31 14:59:41 -0300

    Improved demo page and generation script

--- gulpfile.js
+++ gulpfile.js
@@ -8,16 +8,17 @@ const concat = require("gulp-concat");
 
 var distBase = "./dist";
 var svgSource = "./src/svg";
-gulp.task("clear-dist", function (cb) {
-   
+gulp.task("clear-dist", function(cb) {
+
 });
 
-gulp.task("generate-font", function (done) {
+gulp.task("generate-font", function(done) {
     var config = {
-        fontName: 'myfont', // required 
-        prependUnicode: true, // recommended option 
+        fontName: 'DemoFont',
+        prependUnicode: true,
         formats: ['ttf', 'eot', 'woff', 'woff2', 'svg'],
-        normalize: true
+        normalize: true,
+        fontHeight: 1001
     };
 
     var iconStream = gulp.src(path.join(svgSource, "*.svg"))
@@ -25,15 +26,15 @@ gulp.task("generate-font", function (done) {
 
     async.parallel([
         function buildStyles(cb) {
-            iconStream.on('glyphs', function (glyphs, options) {
+            iconStream.on('glyphs', function(glyphs, options) {
                 var consolidateStream = consolidate('lodash', {
                     glyphs: glyphs,
                     fontName: config.fontName,
                     fontPath: '../fonts/',
-                    className: 's'
+                    className: config.fontName.toLowerCase()
                 });
 
-                gulp.src('./src/templates/font-style.css')
+                gulp.src('./src/templates/style.css')
                     .pipe(consolidateStream)
                     .pipe(concat(config.fontName + ".css"))
                     .pipe(gulp.dest(path.join(distBase, "css")))
@@ -41,15 +42,15 @@ gulp.task("generate-font", function (done) {
             });
         },
         function buildDemo(cb) {
-            iconStream.on('glyphs', function (glyphs, options) {
+            iconStream.on('glyphs', function(glyphs, options) {
                 var consolidateStream = consolidate('lodash', {
                     glyphs: glyphs,
                     fontName: config.fontName,
                     fontPath: '../fonts/',
-                    className: 's'
+                    className: config.fontName.toLowerCase()
                 });
 
-                gulp.src('./src/templates/font-demo.html')
+                gulp.src('./src/templates/demo.html')
                     .pipe(consolidateStream)
                     .pipe(concat(config.fontName + ".html"))
                     .pipe(gulp.dest(distBase))

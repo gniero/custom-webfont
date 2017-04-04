fcb4d190d0ddefe1d47626d6a39d4ac5bb461f48
Guilherme Niero <gniero@gmail.com>
2017-04-04 08:54:43 -0300

    Minor adjustments on gulpfile and demo page

--- gulpfile.js
+++ gulpfile.js
@@ -8,14 +8,14 @@ const concat = require("gulp-concat");
 
 var distBase = "./dist";
 var svgSource = "./src/svg";
-gulp.task("clear-dist", function(cb) {
-
+gulp.task("clear-dist", function (cb) {
+    del(distBase).then(() => cb());
 });
 
-gulp.task("generate-font", function(done) {
+gulp.task("generate-font", ["clear-dist"], function (done) {
     var config = {
         fontName: 'DemoFont',
-        prependUnicode: true,
+        prependUnicode: false,
         formats: ['ttf', 'eot', 'woff', 'woff2', 'svg'],
         normalize: true,
         fontHeight: 1001
@@ -26,7 +26,7 @@ gulp.task("generate-font", function(done) {
 
     async.parallel([
         function buildStyles(cb) {
-            iconStream.on('glyphs', function(glyphs, options) {
+            iconStream.on('glyphs', function (glyphs, options) {
                 var consolidateStream = consolidate('lodash', {
                     glyphs: glyphs,
                     fontName: config.fontName,
@@ -42,7 +42,7 @@ gulp.task("generate-font", function(done) {
             });
         },
         function buildDemo(cb) {
-            iconStream.on('glyphs', function(glyphs, options) {
+            iconStream.on('glyphs', function (glyphs, options) {
                 var consolidateStream = consolidate('lodash', {
                     glyphs: glyphs,
                     fontName: config.fontName,
